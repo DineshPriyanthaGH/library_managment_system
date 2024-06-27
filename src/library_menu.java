736fef135ae8b3cade74772387159e0c73b4f989
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-27 14:09:39 +0530

    import get user input class and create scanner new object

--- src/library_menu.java
+++ src/library_menu.java
@@ -1,2 +1,9 @@
+import java.util.Scanner; //import get user input class
+
 public class library_menu {
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in); // create scanner new object
+    }
+
+
 }
