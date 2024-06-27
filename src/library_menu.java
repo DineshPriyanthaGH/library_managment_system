ea4f025ccf57f751f70d2c6c1fbf74a3c95e255b
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-27 14:20:32 +0530

    "choice" variable declare to store user input

--- src/library_menu.java
+++ src/library_menu.java
@@ -3,6 +3,7 @@ import java.util.Scanner; //import get user input class
 public class library_menu {
     public static void main(String[] args){
         Scanner scanner= new Scanner(System.in); // create scanner new object
+         int choice; //  "choice" variable declare to store user input
     }
 
 
