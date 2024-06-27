371dfafee864fbb673c192f1e2576609ce49ddcb
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-06-27 12:12:36 +0530

    Variables for URL,User and password to connect

--- src/jdbc.java
+++ src/jdbc.java
@@ -5,4 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException; // import the exception class
 
 public class jdbc {
+    // Variables for URL,User and password to connect
+    private static final String URl= "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root1234";
+
+
+
+
+
+
 }
