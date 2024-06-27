d25779caec9b240e62c5bbd66177dd33ccd4a6c3
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-27 14:28:11 +0530

    create loop for user can choose choice

--- src/library_menu.java
+++ src/library_menu.java
@@ -4,6 +4,12 @@ public class library_menu {
     public static void main(String[] args){
         Scanner scanner= new Scanner(System.in); // create scanner new object
          int choice; //  "choice" variable declare to store user input
+
+        // create loop for user can choose choice
+        do{
+
+        }while();
+
     }
 
 
