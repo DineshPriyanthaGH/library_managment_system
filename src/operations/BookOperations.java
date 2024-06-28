536c610a6c8191a6f2b36a7ce8629483d54570f0
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-28 09:19:36 +0530

    import sql connection classes

--- src/operations/BookOperations.java
+++ src/operations/BookOperations.java
@@ -2,4 +2,10 @@ package operations;// inside package operations
 
 import db.jdbc; // import db connectivity packages
 
+//import sql connection classes
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
