7b3968e1e5274c349024bc795a4c8435b615c810
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-29 12:58:49 +0530

    import dates class

--- src/operations/LoanOperations.java
+++ src/operations/LoanOperations.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Date;
+import java.sql.Date; // import dates class to handling sql dates
 
 public class LoanOperations {
 }
