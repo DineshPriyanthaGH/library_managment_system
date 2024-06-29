de49428f7e00deca68b2f2f4a6377b6ec1f483a1
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-29 13:45:31 +0530

    member_id parameter in sql statement

--- src/operations/LoanOperations.java
+++ src/operations/LoanOperations.java
@@ -31,6 +31,8 @@ public class LoanOperations {
             PreparedStatement stmt = conn.prepareStatement(query); //sql statement
 
             stmt.setInt(1, book_id);//book_id parameter in sql statement
+            stmt.setInt(2, member_id); //member_id parameter in sql statement
+
 
 
 
