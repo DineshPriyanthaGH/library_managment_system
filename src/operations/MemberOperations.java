c9a56c570325d3c523389f579ec74bd2efe7810f
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-29 11:26:04 +0530

    import sql connection classes

--- src/operations/MemberOperations.java
+++ src/operations/MemberOperations.java
@@ -1,6 +1,12 @@
 package operations;
-
-
 import db.jdbc; // import db connectivity packages
+
+//import sql connection classes
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class MemberOperations {
 }
