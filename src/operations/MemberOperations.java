399f150243c110618ca26a1b00612a4fdb6693e8
DineshPriyanthaGH <dineshpriyantha200248@gmail.com>
2024-06-29 11:26:27 +0530

    import user input get class

--- src/operations/MemberOperations.java
+++ src/operations/MemberOperations.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.Scanner; // import user input get class
 
 public class MemberOperations {
 }
